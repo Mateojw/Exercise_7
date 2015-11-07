518610886212ed8de109d50eb75910fa84477294
Mateojw <mjw557@gmail.com>
2015-11-07 16:15:17 -0500

    this is the draft 2 of framework i am going to use this to make the visualisation
    draft 2 I am still getting load errors in all browsers and think it
    could be related to my html.

---
+++ Ex 7 draft 2/rats.js
@@ -0,0 +1,20 @@
+
+this is heavaly refrenced from https://www.youtube.com/watch?v=118sDpLOClw&list=PLRqwX-V7Uu6a-SQiI4RtIwuOrLJGnel0r p5.js tutorial - 8.3 What is JSON (JavaScript Object Notation)? (Part 2) with Daniel Shiffman i am using this as a basis to get information to later visualise the data.
+ var data;
+function preload(){
+	data = loadJSON(birds.json);// loads in json file birds 
+}
+
+function setup() {
+    noCanvas();//no canvas 
+	
+	var birds = data.birds;// the first part of the json file showing all the main catagories of the birds an array of brids 
+	
+	for (var i = 0; i < birds.length; i++) {//running through each major group of birds // the birds
+		createElement('h1', birds[i].family);//show each major group of birds 
+		var members = birds[i].members; //sets each member to var members
+		for (var f = 0; f < members.length; f++) {//running through the length of the sub groups//the members  
+			createDiv(members[i]);//show the sub groups of birds underneath
+		}
+	}
+}
